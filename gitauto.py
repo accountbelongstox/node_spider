d8c41bc3caf0b65478c3caffeba272649852c0de
accountbelongstox <accountbelongstox@163.com>
2024-03-06 13:53:57 +0800

    2024-03-06 13:53:56

--- gitauto.py
+++ gitauto.py
@@ -141,7 +141,7 @@ git diff --name-only --diff-filter=U
                 git_dir = os.path.join(item_path, '.git')
                 if not_skip(item):
                     if os.path.isdir(git_dir):
-                        self.print_info(f"Git-Dir:{git_dir}")
+                        self.print_info(f"Git-Dir\t:{git_dir}")
                         self.git_commmit_directory(item_path)
                     self.scan_directory(item_path,skip_dirs)
                 
